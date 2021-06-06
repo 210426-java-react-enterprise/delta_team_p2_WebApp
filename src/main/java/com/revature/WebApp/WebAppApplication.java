ed89a2a39bb8974579722795a3099ea67f218704
DanceableBleats <KylePlummerBSCS@gmail.com>
2021-06-06 18:31:14 -0400

    Create, Read, and Update methods for a prototype servlet created. Controller, Entity, and Repository.

--- src/main/java/com/revature/WebApp/WebAppApplication.java
+++ src/main/java/com/revature/WebApp/WebAppApplication.java
@@ -3,10 +3,11 @@ package com.revature.WebApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WebAppApplication {
-
 	public static void main(String[] args) {
+
 		SpringApplication.run(WebAppApplication.class, args);
 	}
 
